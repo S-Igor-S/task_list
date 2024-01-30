568667f6421a886ef3b6b969225634d71e90e91c
Ihor Prachuk <ihor.prachuk@gmail.com>
2024-01-30 14:22:56 +0100

    feat: added feature for edit tasks

--- routes/web.php
+++ routes/web.php
@@ -15,11 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::view('/create', 'create')->name('tasks.create');
+
 Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
-
-Route::view('/create', 'create')->name('tasks.create');
 
 Route::get('/tasks', function ()  {
     return view('index', [
@@ -31,6 +31,12 @@ Route::get('/tasks/{id}', function ($id) {
 
     return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', [
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.edit');
 
 Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
@@ -48,3 +54,20 @@ Route::post('/tasks', function (Request $request) {
     return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
