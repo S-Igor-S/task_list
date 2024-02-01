2bd17670d9f05f963e9778c915cf0c4833452df1
Ihor Prachuk <ihor.prachuk@gmail.com>
2024-02-01 10:30:37 +0100

    feat: added pagination

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,4 +12,9 @@
         <div>There are no tasks!</div>
     @endforelse
 </div>
+    @if ($tasks->count())
+        <nav>
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
